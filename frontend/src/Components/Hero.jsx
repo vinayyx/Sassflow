5c52308c189d8054847c2ccae3c0566845c1187b
Vinay Kushwah <kushwahvinay95@gmail.com>
2025-08-07 03:58:40 +0530

    remove error

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from '../ui/button'
+import Button from '../ui/Button'
 import { useNavigate } from 'react-router-dom'
 import TrustedTestimonials from '../ui/TrustedTestimonials'
 import Brands from './Brands'
